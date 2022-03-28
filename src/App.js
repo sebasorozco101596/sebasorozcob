f28fd38c63d8850b3589791d3527c374cd3a0cd3
Juan S Orozco Buitrago <Sebasorozcob@gmail.com>
2022-03-28 15:38:42 -0400

    Adding the Restaurant page to my personal website

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Main from './components/main'
 import Programmer from '../src/programmer/programmer'
 import Photography from '../src/photography/photography'
+import RestaurantApp from './web_apps/restaurant/RestaurantApp';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Main />} />
         <Route path="/programmer" element={<Programmer />} />
         <Route path="/photography" element={<Photography />} />
+        <Route path="/restaurant" element={<RestaurantApp />} />
       </Routes>
     </Router>
   );
---
+++ src/web_apps/restaurant/RestaurantApp.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
+import { Navbar } from './components';
+import './RestaurantApp.css';
+
+const RestaurantApp = () => (
+
+  <body id='restaurant'>
+    <div>
+      <Navbar />
+      <Header />
+      <AboutUs />
+      <SpecialMenu />
+      <Chef />
+      <Intro />
+      <Laurels />
+      <Gallery />
+      <FindUs />
+      <Footer />
+    </div>
+  </body>
+);
+
+export default RestaurantApp;
